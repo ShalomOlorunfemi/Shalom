964760c9dbe160c3b51404d0aefc6d5be91acd9f
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-26 11:00:14 +0100

    Added some more functionality to College Course class

--- ex02/CollegeCourse.java
+++ ex02/CollegeCourse.java
@@ -3,10 +3,16 @@ public class CollegeCourse
 	public static void main(String[] args)
 	{
 		Student Shalom = new Student();
+			System.out.println("The object is: "+Shalom+"\n\n");
 		Student Eoin = new Student();
+			System.out.println("The object is: "+Eoin+"\n\n");
 		Student Armand = new Student();
+			System.out.println("The object is: "+Armand+"\n\n");
 		Student Mansur = new Student();
+			System.out.println("The object is: "+Mansur+"\n\n");
 		Student Shane = new Student();
+			System.out.println("The object is: "+Shane+"\n\n");
 		Student Paddy = new Student();
+			System.out.println("The object is: "+Paddy+"\n\n");
 	}	
 }
