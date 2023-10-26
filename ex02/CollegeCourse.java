0144acbb329e6a3303b4f3ebd14b7d95009a564c
Mark Deegan <mark.deegan@tudublin.ie>
2023-10-26 10:54:51 +0100

    Added College Course.java Added Constructor to class Student

---
+++ ex02/CollegeCourse.java
@@ -0,0 +1,12 @@
+public class CollegeCourse
+{	
+	public static void main(String[] args)
+	{
+		Student Shalom = new Student();
+		Student Eoin = new Student();
+		Student Armand = new Student();
+		Student Mansur = new Student();
+		Student Shane = new Student();
+		Student Paddy = new Student();
+	}	
+}
